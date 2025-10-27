80a61fc8591e74f5d3258c4b146307765762f810
RockyXiong47 <rockxiong83@gmail.com>
2025-10-27 16:00:29 -0500

    added json simple

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module org.metrostate.ics.ordertrackingapp {
     requires javafx.controls;
-    requires javafx.fxml;
+    requires javafx.fxml; requires json.simple;
 
 
     opens org.metrostate.ics.ordertrackingapp to javafx.fxml;
