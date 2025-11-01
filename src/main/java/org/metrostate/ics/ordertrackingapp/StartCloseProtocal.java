9d44f9193c5c9025c9873cd98ec038416246903d
RockyXiong47 <rockxiong83@gmail.com>
2025-11-01 16:29:40 -0500

    added some place holder methods

--- src/main/java/org/metrostate/ics/ordertrackingapp/StartCloseProtocal.java
+++ src/main/java/org/metrostate/ics/ordertrackingapp/StartCloseProtocal.java
@@ -1,4 +1,22 @@
 package org.metrostate.ics.ordertrackingapp;
 
 public class StartCloseProtocal {
+    public void foodHubStart (){
+        //place holder
+    }
+
+    public void foodHubClose(){
+        //place holder
+    }
+
+    public boolean saveOrder(){
+        //place holder
+        return false;
+    }
+
+    public boolean loadOrder(){
+        //place holder
+        return false;
+    }
+
 }
