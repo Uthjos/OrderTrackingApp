dcdc58401089cf004c8d13a0c34a83279af32541
Joseph Murtha <josephmurtha@gmail.com>
2025-11-02 15:40:16 -0600

    initial test template

---
+++ src/test/java/org/metrostate/ics/ordertrackingapp/FoodItemTest.java
@@ -0,0 +1,36 @@
+package org.metrostate.ics.ordertrackingapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FoodItemTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getQuantity() {
+    }
+
+    @Test
+    void setQuantity() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void setPrice() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
