9b5a65848f13818ebf532ba009e8e73c737493ec
Ashley Zenzola <175030924+azenzola8@users.noreply.github.com>
2025-11-02 18:04:18 -0600

    Added comments

--- src/main/java/org/metrostate/ics/ordertrackingapp/Launcher.java
+++ src/main/java/org/metrostate/ics/ordertrackingapp/Launcher.java
@@ -2,6 +2,9 @@ package org.metrostate.ics.ordertrackingapp;
 
 import javafx.application.Application;
 
+/**
+ * Launches the program.
+ */
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(OrderTrackerApp.class, args);
