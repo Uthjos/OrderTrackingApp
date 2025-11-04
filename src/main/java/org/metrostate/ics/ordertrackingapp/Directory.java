bdb2b3a1680789738224a46e760dc85af2b14fc7
bannaIsBanna <rockxiong83@gmail.com>
2025-11-03 20:33:17 -0600

    Current Status parser/ our export parser in Progress

--- src/main/java/org/metrostate/ics/ordertrackingapp/Directory.java
+++ src/main/java/org/metrostate/ics/ordertrackingapp/Directory.java
@@ -4,11 +4,16 @@ import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * The Directory Enum holds all data directories
+ */
 public enum Directory {
     savedOrders("orderFiles/savedOrders"),
     testOrders("orderFiles/testOrders"),
     importOrders("orderFiles/importOrders");
-
+    /**
+     * holds the path after src/main/
+     */
     private final String path;
 
     Directory(String path) {
@@ -19,6 +24,11 @@ public enum Directory {
         return path;
     }
 
+    /**
+     * Will create the directory if it doesn't exist
+     * @param directory
+     * @return
+     */
     private static String createDirectory(String directory) {
 
         File f = new File(directory);
@@ -29,6 +39,11 @@ public enum Directory {
         return directory;
     }
 
+    /**
+     * will return the directory of the enum Directory
+     * @param directory
+     * @return
+     */
     public static String getDirectory(Directory directory) {
         Path dirPath  = Paths.get(System.getProperty("user.dir"), "src", "main")
                 .resolve(directory.getPath());
